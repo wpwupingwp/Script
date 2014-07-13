25a076d0fdebea87271f6dc5fd6759df5bc70adc
wuping <wpwupingwp@hotmail.com>
2014-07-13 11:33:22 +0800

    fix syntax error of 'if'

--- assembly.sh
+++ assembly.sh
@@ -1,12 +1,14 @@
-rename 's/\.fasta//' *.fasta
+rename 's/\.fasta//' ./*.fasta
 for a in *
 do
-    runAssembly -o $a+"_" -p $a
+    #runAssembly -o $a"_" -p $a
+    echo '1'
 done
 
 for d in *
 do
-    if [-d "$d"]
+    if [ -d "$d" ]
+    then
         {
             cd $d
             sed -i 's/>/>'"$d"'/' 454AllContigs.fna
