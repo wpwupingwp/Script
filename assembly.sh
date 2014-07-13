4678329209d9a6ed96f26bbda127e8a6e4a7b4ce
wuping <wpwupingwp@hotmail.com>
2014-07-13 11:23:14 +0800

    temp version

--- assembly.sh
+++ assembly.sh
@@ -1,14 +1,17 @@
-for a in *.fasta
+rename 's/\.fasta//' *.fasta
+for a in *
 do
-    runAssembly -o $a -p $a
+    runAssembly -o $a+"_" -p $a
 done
 
 for d in *
 do
     if [-d "$d"]
-        cd $d
-        replace()
-        sed -i 's/>/>'"$d"'/' *.fna
-        sed -i 's/>/>'"$d"'/' *.qual
+        {
+            cd $d
+            sed -i 's/>/>'"$d"'/' 454AllContigs.fna
+            sed -i 's/>/>'"$d"'/' 454AllContigs.qual
+            cd ..
+        }
     fi
 done
