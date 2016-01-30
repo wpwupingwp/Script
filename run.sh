a58cf239bbb93780c2e17dc40dc32ce502f3f126
Ping Wu (Debian) <wpwupingwp@hotmail.com>
2016-01-30 15:24:51 +0800

    fix variants

--- run.sh
+++ run.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-parameter=parameter.txt
-info=info.csv
-left=left.fastq
-right=right.fastq
-output="output"
-thread=16
+info=barcode_primer.csv
+L=left.fastq
+R=right.fastq
+merge=merged.fastq
 
-cat "$parameter" >> "$info"
+output="output"
+thread=8
+parameter=parameter.txt
+
+cat "$parameter" "$info" >> info.txt
 mkdir "$output"
 
 echo "Merge"
-./flash "$left" "$right"
+./flash "$L" "$R"
 python3 join_fastq.py out.notCombined_1.fastq out.notCombined_2.fastq
-cat out.extendedFrags.fastq combine.fastq > merged.fastq
+cat out.extendedFrags.fastq combine.fastq > "$merge"
 echo "Check"
-perl ./ampliCHECK.pl -"$thread" 16 -i ./merged.fastq -d "$info" -t 'Illumina' -o "$output"/check > "$output"/check.log
+perl ./ampliCHECK.pl -thr "$thread" -i "$merge" -d info.txt -t 'Illumina' -o "$output"/check > "$output"/check.log
 echo "SAS"
-perl ./ampliSAS.pl -"$thread" 16 -i ./merged.fastq -d "$info" -t 'Illumina' -o "$output"/SAS > "$output"/sas.log
+perl ./ampliSAS.pl -thr "$thread" -i "$merge" -d info.txt -t 'Illumina' -o "$output"/SAS > "$output"/sas.log
 echo "Compare"
 perl ./ampliCOMPARE.pl -1 "$output"/check/results.xlsx -2 "$output"/SAS/results.xlsx -o "$output"/compare.xlsx > "$output"/compare.log
