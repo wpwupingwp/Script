701035fdda357490eb297a2490bcd5594a608289
Ping Wu <wpwupingwp@hotmail.com>
2016-02-27 21:08:57 +0800

    fix merge-cut

--- run.sh
+++ run.sh
@@ -18,11 +18,11 @@ echo "Merge"
 python3 join_fastq.py out.notCombined_1.fastq out.notCombined_2.fastq
 cat out.extendedFrags.fastq combine.fastq > "$merge"
 echo "Cut"
-python3 cut_barcode.py "$merge" -o "$merge-cut" -f info.txt
+python3 cut_barcode.py "$merge" -o "$merge"-cut -f info.txt
 echo "SAS"
-perl ./ampliSAS.pl -thr "$thread" -i "$merge-cut" -d info.txt -t 'Illumina' -o "$output"/sas > "$output"/sas.log
+perl ./ampliSAS.pl -thr "$thread" -i "$merge"-cut -d info.txt -t 'Illumina' -o "$output"/sas > "$output"/sas.log
 echo "Check"
-perl ./ampliCHECK.pl -thr "$thread" -i "$merge-cut" -d info.txt -t 'Illumina' -o "$output"/check > "$output"/check.log
+perl ./ampliCHECK.pl -thr "$thread" -i "$merge"-cut -d info.txt -t 'Illumina' -o "$output"/check > "$output"/check.log
 echo "Compare"
 perl ./ampliCOMPARE.pl -1 "$output"/check/results.xlsx -2 "$output"/SAS/results.xlsx -o "$output"/compare.xlsx > "$output"/compare.log
 echo "Rename"
