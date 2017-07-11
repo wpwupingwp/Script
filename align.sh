12bfe9abf37ca153dd134c66fe391782a0331371
Workstation <wpwupingwp@outlook.com>
2017-07-11 10:01:47 +0800

    update align

--- align.sh
+++ align.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in *.fasta
 do
-    time mafft --reorder --adjustdirection --thread 15 --genafpair ${i} > ${i}.aln | tee ${i}.log
+    time mafft --reorder --adjustdirection --thread 15 --genafpair --maxiterate 1000 ${i} > ${i}.aln | tee ${i}.log
 done
